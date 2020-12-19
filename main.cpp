fb62c731d5ea732445c6ef363895aa4018a0f6d0
Donneboe <donneboe@gmail.com>
2020-12-18 20:49:54 -0800

    Final Update

--- main.cpp
+++ main.cpp
@@ -4,36 +4,41 @@
 
 #include <igl/collapse_edge.h>
 #include <igl/edges.h>
+#include <igl/edge_flaps.h>
+#include <igl/edge_collapse_is_valid.h>
 #include <igl/min_heap.h>
 
 Eigen::MatrixXd V, OV;
 Eigen::MatrixXi F, OF;
 Eigen::VectorXi j;
 
-igl::min_heap<std::tuple<double, int , int>> Q;
-std::vector<std::pair<Eigen::Vector4d, int>> Vhats;
-Eigen::Matrix<int, Eigen::Dynamic, 2> E;
+// To have this run, change this to your own directory.
+#define bunny_path "C:/Users/Donny/OneDrive/Documents/GitHub/CS112Mesh/bunny10k.ply"
 
-void edge_Collapse(
-  Eigen::MatrixXd &V, Eigen::MatrixXi &F,
+bool edge_Collapse(
+  Eigen::MatrixXd &ov, Eigen::MatrixXi &of,
   Eigen::Vector4d Vh, std::pair<int, int> e)
   {
-
     Eigen::MatrixXd NV;
     Eigen::MatrixXi NF;
     int r = 0;
     int vi;
     bool newV_added = false;
     Eigen::Vector3d vh = Vh.head<3>();
-    NV.resize(V.rows()-1, 3);
-    NF.resize(F.rows()-2, 3);
+    NV.resize(ov.rows(), 3);
+    NF.resize(of.rows(), 3);
 
-    for (unsigned int i = 0; i < V.rows(); i++){
+    Eigen::VectorXi EMAP;
+    Eigen::MatrixXi E,EF,EI;
+    igl::edge_flaps(of,E,EMAP,EF,EI);
+    if (!igl::edge_collapse_is_valid(e.first,of,E, EMAP, EF, EI))
+    {
+      return false;
+    }
+
+    for (unsigned int i = 0; i < ov.rows(); i++){
       if (i==e.first || i==e.second){
-        if (newV_added){
-
-        }
-        else{
+        if (!newV_added){
           newV_added = true;
           NV.row(r) = vh;
           vi = r;
@@ -41,56 +46,65 @@ void edge_Collapse(
         }
       }
       else{
-        NV.row(r) = V.row(i);
+        NV.row(r) = ov.row(i);
         r++;
       }
     }
+    NV.conservativeResize(r, 3);
     r = 0;
 
-    for (unsigned int i = 0; i < F.rows(); i++){
-      if ((F(i, 0)==e.first && F(i, 1)==e.second)
-      || (F(i, 0)==e.second && F(i, 1)==e.first)
-      || (F(i, 0)==e.first && F(i, 2)==e.second)
-      || (F(i, 0)==e.second && F(i, 2)==e.first)
-      || (F(i, 1)==e.first && F(i, 2)==e.second)
-      || (F(i, 1)==e.second && F(i, 2)==e.first))
+    for (unsigned int i = 0; i < of.rows(); i++){
+      if ((of(i, 0)==e.first && of(i, 1)==e.second)
+      || (of(i, 0)==e.second && of(i, 1)==e.first)
+      || (of(i, 0)==e.first && of(i, 2)==e.second)
+      || (of(i, 0)==e.second && of(i, 2)==e.first)
+      || (of(i, 1)==e.first && of(i, 2)==e.second)
+      || (of(i, 1)==e.second && of(i, 2)==e.first))
       {
 
       }
-      else if ((F(i, 0)==e.first || F(i, 0)==e.second)
-      || (F(i, 1)==e.first || F(i, 1)==e.second)
-      || (F(i, 2)==e.first || F(i, 2)==e.second)){
-        if ((F(i, 0)==e.first || F(i, 0)==e.second)){
-          NF.row(r) = F.row(i);
+      else if ((of(i, 0)==e.first || of(i, 0)==e.second)
+      || (of(i, 1)==e.first || of(i, 1)==e.second)
+      || (of(i, 2)==e.first || of(i, 2)==e.second)){
+        if ((of(i, 0)==e.first || of(i, 0)==e.second)){
+          NF.row(r) = of.row(i);
           NF(r, 0) = vi;
           r++;
         }
-        else if ((F(i, 1)==e.first || F(i, 1)==e.second)){
-          NF.row(r) = F.row(i);
+        else if ((of(i, 1)==e.first || of(i, 1)==e.second)){
+          NF.row(r) = of.row(i);
           NF(r, 1) = vi;
           r++;
         }
-        else if ((F(i, 2)==e.first || F(i, 2)==e.second)){
-          NF.row(r) = F.row(i);
+        else if ((of(i, 2)==e.first || of(i, 2)==e.second)){
+          NF.row(r) = of.row(i);
           NF(r, 2) = vi;
           r++;
         }
       }
       else{
-        NF.row(r) = F.row(i);
+        NF.row(r) = of.row(i);
         r++;
       }
     }
-    V = NV;
-    F = NF;
+
+    NF.conservativeResize(r, 3);
+    ov = NV;
+    of = NF;
+    return true;
   }
 
 
-void computeOptimalContract(
+std::pair<igl::min_heap<std::tuple<double, int , int>>, std::vector<std::pair<Eigen::Vector4d, int>>>
+computeOptimalContract(
   std::vector<std::pair<Eigen::MatrixXd, int>> Qs,
-  Eigen::Matrix<int, Eigen::Dynamic, 2>& E)
+  Eigen::Matrix<int, Eigen::Dynamic, 2>& E
+  )
 {
-  
+  igl::min_heap<std::tuple<double, int , int>> Q;
+  std::vector<std::pair<Eigen::Vector4d, int>> Vhats;
+  std::pair<igl::min_heap<std::tuple<double, int , int>>, std::vector<std::pair<Eigen::Vector4d, int>>> qv;
+
   Eigen::Vector4d Vhat, I(0,0,0,1);
   Eigen::Matrix4d Q1, Q2, Qhat, cry;
 
@@ -140,6 +154,10 @@ void computeOptimalContract(
     vh.second = i;
     Vhats.push_back(vh);
   }
+
+  qv.first = Q;
+  qv.second = Vhats;
+  return qv;
 }
 
 std::vector<std::pair<Eigen::MatrixXd, int>> computeQ(Eigen::MatrixXd& V, Eigen::MatrixXi& F)
@@ -210,46 +228,64 @@ std::vector<std::pair<Eigen::MatrixXd, int>> computeQ(Eigen::MatrixXd& V, Eigen:
 }
 
 void Decimate(Eigen::MatrixXd &V, Eigen::MatrixXi &F, int m_f){
+  Eigen::Matrix<int, Eigen::Dynamic, 2> E;
+  igl::min_heap<std::tuple<double, int , int>> Q;
+  std::vector<std::pair<Eigen::Vector4d, int>> Vhats;
+  std::pair<igl::min_heap<std::tuple<double, int , int>>, std::vector<std::pair<Eigen::Vector4d, int>>> qv;
   std::tuple<double, int , int> p;
   std::pair<Eigen::Vector4d, int> Vh;
   std::pair<int, int> e;
   Eigen::Vector4d vh;
   int m = 0;
+  Eigen::MatrixXd ov = V;
+  Eigen::MatrixXi of = F;
+
   while(true){
-    if (F.rows() <= m_f){
+    if (of.rows() <= m_f){
       break;
     }
     else if (m==10000){
       break;
     }
     else{
-      igl::edges(F, E);
-      computeOptimalContract(computeQ(V,F), E);
+      igl::edges(of, E);
+      qv = computeOptimalContract(computeQ(ov,of), E);
+      Q = qv.first;
+      Vhats = qv.second;
       if (Q.size()==0){
         break;
       }
-      p = Q.top();
-      Q.pop();
-      for (unsigned int i = 0; i<Vhats.size(); i++){
-        if (Vhats[i].second == std::get<1>(p)){
-          Vh = Vhats[i];
+      while(true)
+      {
+        p = Q.top();
+        Q.pop();
+        for (unsigned int i = 0; i<Vhats.size(); i++){
+          if (Vhats[i].second == std::get<1>(p)){
+            Vh = Vhats[i];
+            break;
+          }
+        }
+        e.first = E(std::get<1>(p), 0);
+        e.second = E(std::get<1>(p), 1);
+        vh = Vh.first;
+        bool collapse = edge_Collapse(ov,of,vh,e);
+        if (collapse)
+        {
           break;
         }
       }
-      e.first = E(std::get<1>(p), 0);
-      e.second = E(std::get<1>(p), 1);
-      vh = Vh.first;
-      edge_Collapse(V,F,vh,e);
     }
   m++;
+  std::cout << "Current Iterations " << m << "\nNumber of faces after edge collapse " << of.rows() << std::endl;
   }
+  V = ov;
+  F = of;
 }
 
 void updateViewer(igl::opengl::glfw::Viewer& viewer)
 {
   viewer.data().clear();
   viewer.data().set_mesh(V, F);
-  viewer.data().set_face_based(true);
 }
 
 // Reset V and F to their original vertices and faces.
@@ -264,7 +300,7 @@ void reset(igl::opengl::glfw::Viewer& viewer)
 int main(int argc, char *argv[])
 {
   // Loads our Bunny mesh
-  igl::readPLY("~/bunny10k.ply", OV, OF);
+  igl::readPLY(bunny_path, OV, OF);
   igl::opengl::glfw::Viewer viewer;
 
   // Plot the mesh
@@ -287,9 +323,10 @@ int main(int argc, char *argv[])
         updateViewer(viewer);
         break;
       case '2':
-        Decimate(V, F, F.size() * .20);
-        updateViewer(viewer);
-        // Colapse_Edge
+        Decimate(V, F, F.rows() * .90);
+        std::cout << "it finishes decimating!" << std::endl;
+        // updateViewer is commented out as it will crash
+        // updateViewer(viewer);
       default: return false;
     }
 
